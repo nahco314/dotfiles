4600974ea4c40f3a5e96423a8120bc03a098efd0
nahco314 <nahco3_ta@yahoo.co.jp>
2024-12-28 20:07:53 +0900

    update

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,6 +5,8 @@ if status is-interactive
     alias ls="lscat"
 end
 ~/.local/bin/mise activate fish | source
-export EMSDK_PYTHON=/home/nahco314/.local/share/uv/python/cpython-3.12.5-linux-x86_64-gnu/bin/python3
-export EMSDK_QUIET=1
+set -x EMSDK_PYTHON /home/nahco314/.local/share/uv/python/cpython-3.12.5-linux-x86_64-gnu/bin/python3
+set -x EMSDK_QUIET 1
 source "/home/nahco314/emsdk/emsdk_env.fish"
+set -x EMSDK_PYTHON /home/nahco314/.local/share/uv/python/cpython-3.12.5-linux-x86_64-gnu/bin/python3
+
