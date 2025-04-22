c514a93a03f70edfeb26a7b66cbfca419bdc0ad0
nahco314 <nahco3_ta@yahoo.co.jp>
2025-04-22 13:47:14 +0900

    update

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
     atuin init fish --disable-up-arrow | source
-    alias ls="lscat"
 end
 ~/.local/bin/mise activate fish | source
 set -x EMSDK_PYTHON /home/nahco314/.local/share/uv/python/cpython-3.12.5-linux-x86_64-gnu/bin/python3
