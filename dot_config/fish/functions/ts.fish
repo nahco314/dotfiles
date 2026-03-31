114830be48deb51e5c46cd923d531a5d68a02d68
nahco314 <nahco3_ta@yahoo.co.jp>
2026-03-31 21:43:50 +0900

    feat: enable true color for local terminals, disable for SSH clients

--- dot_config/fish/functions/ts.fish
+++ dot_config/fish/functions/ts.fish
@@ -2,5 +2,10 @@ function ts
     set session (tmux ls -F "#{session_name}" | fzf \
         --preview 'tmux capture-pane -pt {}' \
         --preview-window 'right:70%')
-    and tmux new-session -t $session
+    or return
+    if set -q SSH_CONNECTION
+        env TERM=screen-256color tmux new-session -t $session
+    else
+        tmux new-session -t $session
+    end
 end
